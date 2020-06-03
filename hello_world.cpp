78d4b606ac54663f6dfe9ddd164156912de7199a
AndreyBMWX6 <a_s_bykov@mail.ru>
2020-06-04 01:01:25 +0300

    hello_world.cpp updated

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-cout << "Hello World!";
+string Name;
+cin >> Name;
+cout << "Hello World from " << Name << endl;
 return 0;
 }
+
